6bdd0d20b8f4c288eaae01d9322f302cb2f61d27
simarpal.kalsi@gmail.com <simarpal.kalsi@gmail.com>
2019-02-13 15:50:27 -0500

    FIRST COMMIT

---
+++ src/database/JDBCConstants.java
@@ -0,0 +1,16 @@
+package database;
+
+/**
+ * Change the driver,url,username and password according to your needs
+ * 
+ * @author Simar
+ */
+public class JDBCConstants
+{
+
+   public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+   public static final String URL         = "jdbc:mysql://localhost:3306/test1";
+   public static final String USERNAME    = "root";
+   public static final String PASSWORD    = "";
+
+}
